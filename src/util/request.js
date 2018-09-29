5bc12d5069e947ae637947782d64d30a8717b226
Santochance <santochance@qq.com>
2018-09-29 15:41:54 +0800

    feat: add unified fetch-based api for request

---
+++ src/util/request.js
@@ -0,0 +1,55 @@
+import ('es6-promise/auto')
+import fetch from 'isomorphic-fetch'
+
+const request = function (url, options) {
+  // fetch 默认不发送 cookie
+  const defaultOptions = {
+    credientials: 'include',
+  }
+  const newOptions = { ...defaultOptions, ...options }
+  if (
+    newOptions.method === 'POST' ||
+    newOptions.method === 'PUT' ||
+    newOptions.method === 'PATCH'  ||
+    newOptions.method === 'DELETE'
+  ) {
+    // 统一设置 header
+    if (!(newOptions.body instanceof FormData)) {
+      newOptions.headers = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json; charset=utf-8',
+        ...newOptions.headers,
+      }
+      newOptions.body = JSON.stringify(newOptions.body)
+    } else {
+      // newOptions.body is FormData
+      newOptions.headers = {
+        Accept: 'application/json',
+        ...newOptions.headers,
+      }
+    }
+  }
+
+  return fetch(url, newOptions)
+    // fetch 不会根据响应状态码判断请求是否成功
+    // 这里状态码表示请求失败时主动 reject
+    .then(res => {
+      if (res.status >= 200 && res.status < 300 || res.status === 304) {
+        return res
+      }
+      const error = new Error(res.status + ' ' + res.statusText)
+      error.name = res.status
+      error.response = res
+      throw error
+    })
+    .then(res => {
+      // DELETE 请求按惯例是返回 204 请求
+      // 204 请求的响应为空，如果调用 json() 会报
+      if (newOptions.method === 'DELETE' || res.status === 204) {
+        return res.text()
+      }
+      return res.json()
+    })
+}
+
+export default request
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="msg"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/components/HelloWorld.vue'
+import { fetchHello } from '@/service/api'
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  },
+  created() {
+    fetchHello()
+      .then((res) => {
+        this.msg = res.message
+      })
   },
 };
 </script>
