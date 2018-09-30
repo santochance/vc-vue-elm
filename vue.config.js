28796d9cd5b5f0d7522f09f5006669cd30c9a151
Santochance <santochance@qq.com>
2018-09-30 13:20:44 +0800

    chore: add support for auto inject style resources

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,25 @@
+const path = require('path')
 const useMock = require('./src/service/mock/useMock')
 
 module.exports = {
   devServer: {
     before: useMock,
   },
+  chainWebpack: config => {
+    // automatic imports style assets
+    // See: https://cli.vuejs.org/guide/css.html#automatic-imports
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+        types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
+  }
+}
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/styles/mixin.scss'),
+        path.resolve(__dirname, './src/assets/styles/variable.scss'),
+      ],
+    })
 }
