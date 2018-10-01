038c7a301611bba00337e2c711c89d8536217646
Santochance <santochance@qq.com>
2018-10-01 19:38:56 +0800

    feat(comp): add Tag component

---
+++ src/components/common/Tag.vue
@@ -0,0 +1,47 @@
+<template>
+  <span class="tag">
+    <span class="tag__holder">{{ $slots.default[0].text }}</span>
+    <span class="tag__shadow"><slot></slot></span>
+  </span>
+</template>
+
+<script>
+  export default {
+    props: {
+      text: {
+        type: String,
+        default: '',
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tag {
+    position: relative;
+    display: inline-block;
+    padding: 2px 4px;
+    white-space: nowrap;
+    border-radius: 2px;
+    color: #fff;
+  }
+  .tag__holder {
+    color: transparent;
+  }
+  .tag__shadow {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: -100%;
+    bottom: -100%;
+    transform: scale(.5);
+    transform-origin: 0 0;
+
+    color: inherit;
+    font-size: 2em;
+  }
+</style>
---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+import Tabs from './Tabs'
+import Tag from './Tag'
+
+export {
+  Tabs,
+  Tag,
+}
