bc720a79b8c13dbaf457d2510f4cd72ecd4b1ccb
Santochance <santochance@qq.com>
2018-10-03 20:30:20 +0800

    feat: add mutations of cart module

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,2 +1,10 @@
+/* user */
 export const SAVE_USER_ID = 'SAVE_USER_ID'
 export const SAVE_USER = 'SAVE_USER'
+
+/* cart */
+export const SAVE_CART = 'SAVE_CART'
+export const ADD_CART = 'ADD_CART'
+export const REDUCE_CART = 'REDUCE_CART'
+export const CLEAR_CART = 'CLEAR_CART'
+export const INIT_CART = 'INIT_CART'
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,14 +1,119 @@
+import { isEqual } from 'lodash-es'
+import { Toast } from '@/components/common'
+
 import {
   SAVE_USER_ID,
   SAVE_USER,
+
+  SAVE_CART,
+  ADD_CART,
+  REDUCE_CART,
+  CLEAR_CART,
+  INIT_CART,
 } from './mutation-types'
+
+
+function createCartDetail(payload) {
+  const { id, food_id = id, attrs, sku_id, item_id, category_id, name, price, packing_fee, stock, activity, specs, extra, image_path, weight } = payload
+
+  return {
+    activity,
+    attrs,
+    category_id,
+    extra,
+    id: food_id,
+    food_id,
+    image_hash: image_path,
+    item_id,
+    name,
+    packing_fee,
+    price,
+    quantity: 0,
+    sku_id,
+    specs,
+    stock,
+    weight,
+  }
+}
+
+function getCartDetail(cart, { food_id, attrs }) {
+  const entities = [...cart.entities]
+  const index = entities.findIndex((ent) => ent.id === food_id && isEqual(ent.attrs, attrs))
+  return [ entities[index], index, entities ]
+}
 
 export default {
   /* user */
-  [SAVE_USER_ID]: (state, userId) => {
-    state.userId = userId
+  [SAVE_USER_ID]: function (state, user_id) {
+    state.userId = user_id
   },
-  [SAVE_USER]: (state, user) => {
+  [SAVE_USER]: function (state, user) {
     state.user = user
   },
+
+  /* cart */
+  [SAVE_CART]: function (state, { restaurant_id, cart }) {
+    state.cartMap[restaurant_id] = cart
+  },
+  [ADD_CART]: function (state, payload) {
+    const {
+      quantity = 1,
+      restaurant_id,
+      stock,
+    } = payload
+
+    const cartMap = state.cartMap
+    const cart = cartMap[restaurant_id] || (cartMap[restaurant_id] = {})
+    const entities = cart.entities || (cart.entities = [])
+
+    let [entity, entityIndex] = getCartDetail(cart, payload)
+
+    if (!entity) {
+      entity = createCartDetail(payload)
+      entities.push(entity)
+      entityIndex = entities.length
+    }
+
+    const newQuantity = entity.quantity + quantity
+    if (newQuantity > stock) {
+      Toast.open({
+        content: '超过库存，无法添加!',
+        mark: false,
+      })
+    } else {
+      entities.splice(entityIndex, 1, { ...entity, quantity: newQuantity })
+      // entity.quantity = newQuantity
+    }
+
+    // state.cartMap = {...cartMap}
+  },
+  [REDUCE_CART]: function (state, payload) {
+    const  {
+      quantity = 1,
+      restaurant_id,
+    } = payload
+
+    const cartMap = state.cartMap
+    const cart = cartMap[restaurant_id] || (cartMap[restaurant_id] = {})
+    const entities = cart.entities || (cart.entities = [])
+
+    let [entity, entityIndex] = getCartDetail(cart, payload)
+    if (!entity) return
+
+    const newQuantity = entity.quantity - quantity
+    if (newQuantity <= 0) {
+      entities.splice(entityIndex, 1)
+    } else {
+      entities.splice(entityIndex, 1, { ... entity, quantity: newQuantity })
+    }
+  },
+  [CLEAR_CART]: function (state, { restaurant_id }) {
+    const cartMap = state.cartMap
+    const cart = cartMap[restaurant_id] || (cartMap[restaurant_id] = {})
+    cart.entities = []
+  },
+  [INIT_CART]: function (state, { restaurant_id }) {
+    state.cartMap[restaurant_id] = {}
+  },
 }
+
