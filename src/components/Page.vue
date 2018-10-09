bdb2d993962b9b8112905f1c72d5cf2df69442e4
Santochance <santochance@qq.com>
2018-10-09 16:24:37 +0800

    feat(comp): add Page component

---
+++ src/components/Page.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="page__container">
+    <page-header :title="title"
+      :back-btn="backBtn" />
+    <div class="page__main">
+      <slot></slot>
+    </div>
+    <loading-image :visible="loading"></loading-image>
+  </div>
+</template>
+
+<script>
+  import PageHeader from './PageHeader'
+  import LoadingImage from './LoadingImage'
+
+  export default {
+    name: 'Page',
+    components: {
+      PageHeader,
+      LoadingImage,
+    },
+    props: {
+      title: {
+        type: String,
+      },
+      backBtn: {
+        type: Boolean,
+        default: true,
+      },
+      loading: {
+        type: Boolean,
+        default: false,
+      }
+    },
+    data() {
+      return {
+
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .page__container {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+  .page__main {
+    flex: 1 auto;
+  }
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,78 @@
+<template>
+  <header class="page-header">
+    <span v-if="backBtn"
+      class="page-header__left"
+      @click.stop.prevent="onBack">
+      <img src="@/assets/arrow-left.svg" alt="">
+    </span>
+    <h1 class="page-header__title ellipsis"
+      >{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+
+  export default {
+    name: 'PageHeader',
+    components: {
+
+    },
+    props: {
+      title: {
+        type: String,
+      },
+      backBtn: {
+        type: Boolean,
+        default: true,
+      }
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      onBack() {
+        this.$router.back()
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .page-header {
+    position: relative;
+    display: flex;
+    align-items: center;
+    height: 88px;
+    font-size: 36px;
+    background-image: linear-gradient(90deg, #0af, #0085ff);
+    color: #fff;
+  }
+  .page-header__left {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 88px;
+    height: 88px;
+    img {
+      width: 24px;
+    }
+  }
+  .page-header__title {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+    max-width: 50%;
+    margin: auto;
+    text-align: center;
+    font-size: 1em;
+    font-weight: 700;
+    line-height: 88px;
+  }
+
+</style>
