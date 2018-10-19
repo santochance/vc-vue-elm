402fb1c35c05fa26816e195f358c96275c6afe4d
Santochance <santochance@qq.com>
2018-10-19 13:05:04 +0800

    chore: move image from Rating to public/

--- src/views/Shop/Rating/Rating.vue
+++ src/views/Shop/Rating/Rating.vue
@@ -333,7 +333,7 @@
         width: 100%;
         height: 100%;
         border-radius: 50%;
-        background: url('./sprite-avatar.png');
+        background: url('/sprite-avatar.png');
         background-size: 60px;
       }
       & > img {
