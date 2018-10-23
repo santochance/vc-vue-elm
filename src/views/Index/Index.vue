a2b9b1e2ad3eb98ab7f7bd087d8592de8de8eab9
Santochance <santochance@qq.com>
2018-10-23 18:48:02 +0800

    feat: add IndexMainEntry and IndexSkeleton

--- src/views/Index/Index.vue
+++ src/views/Index/Index.vue
@@ -4,7 +4,19 @@
       
     </header>
     <div class="index__search"></div>
-    <div class="index__navs"></div>
+    <div v-if="entryGroupMap.main"
+      class="index__main-entries">
+      <router-link v-for="mainEntry in entryGroupMap.main.entries"
+        :key="mainEntry.id"
+        to=""
+        class="index__main-entry"
+      >
+        <img :src="$getImage(mainEntry.image_hash, mainEntryImgParam)" alt="">
+        <p class="index__main-entry-title">{{ mainEntry.name }}</p>
+      </router-link>
+    </div>
+
+    <IndexSkeleton></IndexSkeleton>
     <div class="index__member"></div>
     <div class="index__banner"></div>
     <IndexShopList></IndexShopList>
@@ -12,11 +24,15 @@
 </template>
 
 <script>
+  import { fetchEntryList } from '@/service/api'
+
+  import IndexSkeleton from './IndexSkeleton'
   import IndexShopList from './IndexShopList'
 
   export default {
     name: 'Index',
     components: {
+      IndexSkeleton,
       IndexShopList,
     },
     props: {
@@ -24,12 +40,55 @@
     },
     data() {
       return {
-        
+        /* remote data */
+        entryGroupMap: {},
+
+        /* image param */
+        mainEntryImgParam: '?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
       }
+    },
+    created() {
+      this.fetchEntryList()
+    },
+    methods: {
+      fetchEntryList() {
+        return fetchEntryList()
+          .then((entryGroupList) => {
+            this.entryGroupMap = entryGroupList.reduce((rst, group) =>
+              ({
+                ...rst,
+                [group.group_name]: group,
+              })
+            , {})
+          })
+      },
     },
   }
 </script>
 
 <style lang="scss" scoped>
-
+  .index {@at-root{
+    
+    .index__main-entries {
+      display: flex;
+      flex-wrap: wrap;
+      overflow: hidden;
+      height: 354px;
+      text-align: center;
+      background-color: #fff;
+    }
+    .index__main-entry {
+      width: 20%;
+      margin-top: 22px;
+      img {
+        display: inline-block;
+      }
+    }
+    .index__main-entry-title {
+      display: block;
+      margin-top: 10px;
+      font-size: 24px;
+      color: #666;
+    }
+  }}
 </style>
---
+++ src/views/Index/IndexSkeleton.vue
@@ -0,0 +1,84 @@
+<script>
+  
+  export default {
+    name: 'IndexSkeleton',
+    components: {
+      
+    },
+    props: {
+      
+    },
+    data() {
+      return {
+        
+      }
+    },
+  }
+</script>
+
+<template>
+  <div class="shell">
+    <header class="shell__header"></header>
+    <section class="shell__section shellAnimation">
+      <div v-for="(n, idx) in 10" :key="idx"
+        class="shell__item"
+      ></div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .shell {@at-root{
+    
+    .shell__header {
+      height: 190px;
+      background: #0096ff;
+    }
+    .shell__section {
+      outline: 1px solid;
+      background-color: #fff;
+      text-align: center;
+    }
+    .shell__item {
+      display: inline-block;
+      width: 20%;
+      margin-top: 22px;
+      &:before,
+      &:after {
+        content: '';
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        background-color: #f4f4f4;
+      }
+      &:before {
+        width: 80px;
+        height: 80px;
+        margin-top: 6px;
+        margin-bottom: 4px;
+        border-radius: 50%;
+      }
+      &:after {
+        width: 60px;
+        height: 28px;
+        margin-top: 10px;
+      }
+    }
+
+  }}
+
+  .shellAnimation {
+    animation: shellPulse 1s infinite;
+  }
+  @keyframes shellPulse {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: .7;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
