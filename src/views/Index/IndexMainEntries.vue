c31050613e92376d267f9558c35d0abd36f43b4e
Santochance <santochance@qq.com>
2018-10-25 14:32:40 +0800

    feat: add IndexMainEntries

---
+++ src/views/Index/IndexMainEntries.vue
@@ -0,0 +1,73 @@
+<script>
+
+  export default {
+    name: 'IndexMainentries',
+    components: {
+
+    },
+    props: {
+      entries: {
+        type: Array,
+      }
+    },
+    data() {
+      return {
+        /* image param */
+        mainEntryImgParam: '?imageMogr/format/webp/thumbnail/!90x90r/gravity/Center/crop/90x90/',
+      }
+    },
+    computed: {
+
+    },
+    methods: {
+
+    },
+  }
+</script>
+
+<template>
+  <div class="b-entries b-entries__box">
+    <div v-if="entries"
+      class="b-entries__main-entries">
+      <router-link v-for="mainEntry in entries"
+        :key="mainEntry.id"
+        to=""
+        class="b-entries__main-entry"
+      >
+        <img :src="$getImage(mainEntry.image_hash, mainEntryImgParam)" alt="">
+        <p class="b-entries__main-entry-title">{{ mainEntry.name }}</p>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+  .b-entries {}
+
+    .b-entries__box {}
+
+    .b-entries__main-entries {
+      display: flex;
+      flex-wrap: wrap;
+      overflow: hidden;
+      height: 354px;
+      text-align: center;
+      background-color: #fff;
+    }
+    .b-entries__main-entry {
+      width: 20%;
+      margin-top: 22px;
+      img {
+        display: inline-block;
+      }
+    }
+    .b-entries__main-entry-title {
+      display: block;
+      margin-top: 10px;
+      font-size: 24px;
+      color: #666;
+    }
+
+</style>
