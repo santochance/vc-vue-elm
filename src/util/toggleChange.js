1d639e5f3460fde78cdad13cefd9c90145a1e448
Santochance <santochance@qq.com>
2018-10-25 08:58:19 +0800

    feat: add IndexShopListFilter sort part

--- src/util/toggleChange.js
+++ src/util/toggleChange.js
@@ -1,27 +1,131 @@
-const defaultOptions = {
-  toggle: true, // 值相同时是否切换为空值
-  nullValue: null, // 使用的空值
-  // type, // 期望数据类型
-  // fromType,
-  // toType,
+import eq from 'lodash-es/eq'
+import findIndex from 'lodash/findIndex'
+// import findKey from 'lodash/findKey'
+
+const debug = true
+
+debug && (window.findIndex = findIndex)
+
+export function toggleValue(source, input, options) {
+  const toggle = options && options.toggle || true
+  
+  if (eq(source, input)) {
+    toggle && (source = null)
+  } else {
+    source = input
+  }
+
+  return source
 }
 
-// 当前只是简单地使用 `===` 比较
-function eq(a, b) {
-  return a === b
+export function toggleArray(source, input, options) {
+  if (!Array.isArray(source)) {
+    throw new TypeError('first argument must be an array!')
+  }
+
+  const toggle = options && options.toggle || true
+  const index = findIndex(source, (o) => eq(o, input))
+  
+  source = [...source]
+  if (index !== -1) {
+    toggle && source.splice(index, 1)
+  } else {
+    source.push(input)
+  }
+
+  return source
 }
+
+export function toggleEntity(source, input, predicate, options) {
+  if (source != null && typeof source !== 'object' && typeof source !== 'function') {
+    throw new TypeError('first argument must be an object')
+  }
+  if (source != null && typeof input !== 'object' && typeof input !== 'function') {
+    throw new TypeError('second argument must be an object')
+  }
+
+  const toggle = options && options.toggle || true
+  const index = findIndex([source], predicate || input)
+  if (index !== -1) {
+    toggle && (source = null)
+  } else {
+    source = input
+  }
+
+  return source
+}
+
+export function toggleEntities(source, input, predicate, options) {
+  if (!Array.isArray(source)) {
+    throw new TypeError('first argument must be an array!')
+  }
+  if (source != null && typeof input !== 'object' && typeof input !== 'function') {
+    throw new TypeError('second argument must be an object')
+  }
+
+  const toggle = options && options.toggle || true
+  const index = findIndex(source, predicate || input)
+
+  source = [...source]
+  if (index !== -1) {
+    toggle && source.splice(index, 1)
+  } else {
+    source.push(input)
+  }
+
+  return source
+}
+
+// function toggleBooleanDict(dict, input, options) {
+//   const toggle = options.toggle
+
+//   if (input in dict) {
+//     toggle && (dict[input] = false)
+//   } else {
+//     dict[input] = true
+//   }
+// }
+
+// function toggleObject(object, input, predicate, options) {
+//   const toggle = options.toggle
+  
+//   // is input in object?
+//   if (typeof input !== 'object' && typeof input !== 'function') {
+//     predicate = (o) => o === input
+//   }
+//   const key = findKey(object, predicate || input)
+  
+//   debug && console.log('key:', key)
+
+//   if (key) {
+//     // remove
+//     toggle && delete object[key]
+//   } else {
+//     // add
+//     object[key] = input
+//   }
+//   return object
+// }
 
 /**
  * 值相同时返回指定空值
- * @param  {any} oldValue    源值
- * @param  {any} newValue      目标值
+ * @param  {any} source    数据源值
+ * @param  {any} input      目标值
  * @param  {Object} options 配置对象
  * @return {any}         返回值
  */
-export default function toggleChange(oldValue, newValue, options) {
-  if (typeof options === 'boolean') {
-    options = { toggle: false }
-  }
-  const { toggle, nullValue } = { ...defaultOptions, ...options }
-  return toggle && eq(oldValue, newValue) ? nullValue : newValue;
-}
+// export default function toggleChange(source, input, predicate, options) {
+//   options = { ...defaultOptions, ...options }
+
+//   if (Array.isArray(source)) {
+//     debug && console.log('mode: array')
+//     return toggleArray(source, input, predicate, options)
+//   }
+//   if (typeof source === 'object' && source || typeof source === 'function') {
+//   debug && console.log('mode: object')
+//     return toggleObject(source, input, predicate, options)
+//   }
+  
+//   debug && console.log('mode: value')
+//   return toggleValue(source, input, options)
+// }
--- src/views/Index/IndexShopListFilter.vue
+++ src/views/Index/IndexShopListFilter.vue
@@ -1,8 +1,9 @@
 <script>
-  import toggleChange from '@/util/toggleChange'
-  
+  import { toggleValue, toggleArray } from '@/util/toggleChange'
+
   const debug = true
-  debug && (window.toggleChange = toggleChange)
+  debug && (window.toggleValue = toggleValue)
+  debug && (window.toggleArray = toggleArray)
 
   export default {
     name: 'IndexShopListFilter',
@@ -18,53 +19,107 @@
       return {
         selectedInsideSorterName: '', // ?String
         selectedSorter: null, // ?Object
-        
-        selectedFilters: {
 
+        sortDropdownVisible: false,
+
+
+        filterDropdownVisible: false,
+        averageCostsGroupItems: [
+          [null, 20], [20, 40], [40, 60], [60, 80], [80, 100], [100, null],
+        ],
+        // selectedFilters: {},
+
+        filterPayload: {
+          quality_union: null,
+          order_by: null,
+          delivery_mode: [],
+          support_ids: [],
+          activity_types: null,
+          cost_range: null,
+          cost_from: null,
+          cost_to: null,
         },
 
-        sortDowndownVisible: false,
-
-        filterPayload: {},
+        deliverModeIconParam: '?imageMogr/format/webp/thumbnail/!24x24r/gravity/Center/crop/24x24/',
       }
     },
     computed: {
       insideSorterList() {
-        return this.filterOptions.inside_sort_filter
+        try {
+          return this.filterOptions.outside.inside_sort_filter
+        } catch(e) {
+          return []
+        }
       },
       outsideSorterList() {
-        return this.filterOptions.outside_sort_filter
+        try {
+          return this.filterOptions.outside.outside_sort_filter
+        } catch(e) {
+          return []
+        }
       },
       uniqueSorterKeys() {
         return this.insideSorterList.concat(this.outsideSorterList)
-          .reduce((unique, sorter) => 
+          .reduce((unique, sorter) =>
             (unique.indexOf(sorter.key) === -1)
               ? [ ...unique, sorter.key ] : unique
           ,[])
+      },
+      filterGroupList() {
+        return this.filterOptions.bar || {}
+      },
+      supportsGroup() {
+        return this.filterGroupList['supports'] || []
+      },
+      activityTypesGroup() {
+        return this.filterGroupList['activity_types'] || []
+      },
+      deliverMode() {
+        return [this.filterGroupList['delivery_mode']]
+      },
+      averageCostsGroup() {
+        const items = this.averageCostsGroupItems
+        const makeDescription = (from, to) =>
+          (to == null) ? `¥${from}以上`
+            : (from == null) ? `¥${to}以下` : `¥${from} - ¥${to}`
+
+        return items.map(([from, to], idx)=> {
+          return ({
+            description: makeDescription(from, to),
+            id: idx + 1,
+            from,
+            to,
+          })
+        })
       },
     },
     watch: {
       // 动态设置 selectedInsideSorterName 初始值
       'filterOptions.inside_sort_filter'(value) {
+        this.initSelectedInsideSorterName(value)
+      },
+    },
+    created() {
+      debug && (window[this.$options.name] = this)
+
+      this.initSelectedInsideSorterName(this.insideSorterList)
+    },
+    methods: {
+      initSelectedInsideSorterName(list) {
         let name
         try {
-          name = value[0].name
+          name = list[0].name
         } catch(e) {
           name = ''
         }
         this.selectedInsideSorterName = name
       },
-    },
-    created() {
-      debug && (window[this.$options.name] = this)
-    },
-    methods: {
       onSorterClick(sorter, position) {
         const filterPayload = this.filterPayload
         const keysToReset = this.uniqueSorterKeys.filter(key => key !== sorter.key)
 
         keysToReset.forEach(key =>filterPayload[key] = null)
-        filterPayload[sorter.key] = toggleChange(filterPayload[sorter.key], sorter.value)
+        filterPayload[sorter.key] = toggleValue(filterPayload[sorter.key], sorter.value)
 
         // 确保能够触发视图更新
         this.filterPayload = { ...filterPayload }
@@ -77,7 +132,8 @@
         }
       },
       // 值相同时返回指定空值
-      toggleChange,
+      toggleValue,
+      toggleArray,
       checkSorterActive(sorter, selectedSorter) {
         if (!selectedSorter) return false
 
@@ -89,20 +145,43 @@
           return compareSorter(sorter, selectedSorter)
         }
       },
+      checkFilteValueActive(value, selected) {
+        if (!selected) return false
+
+        if (Array.isArray(selected)) {
+          return selected.indexOf(value) !== -1
+        } else {
+          return value === selected
+        }
+      },
+      onFilterClear() {
+        this.filterPayload = {
+          ...this.filterPayload,
+          delivery_mode: [],
+          support_ids: [],
+          activity_types: null,
+          cost_range: null,
+        }
+      },
     },
   }
 </script>
 
 <template>
-  <aside v-if="filterOptions" class="b-filter b-filter__box">
+  <aside v-if="filterOptions"
+    class="b-filter b-filter__box">
+    <div v-show="sortDropdownVisible || filterDropdownVisible"
+      class="b-filter__mask"
+      @click="(sortDropdownVisible = false, filterDropdownVisible = false)"
+    ></div>
     <div class="b-filter__header">
       <a href="javascript:"
-        class="b-filter__nav" 
+        class="b-filter__nav ellipsis"
         :class="{
           'b-filter__nav_active': checkSorterActive(insideSorterList, selectedSorter),
-          'b-filter__nav_open': sortDowndownVisible,
+          'b-filter__nav_open': sortDropdownVisible,
         }"
-        @click="sortDowndownVisible = !sortDowndownVisible"
+        @click="sortDropdownVisible = !sortDropdownVisible"
       >
         <span>{{ selectedInsideSorterName }}</span>
         <!-- <svg><use></use></svg> -->
@@ -116,43 +195,83 @@
         <span>{{ sorter.name }}</span>
       </a>
       <a href="javascript:"
-        class="b-filter__nav" 
+        class="b-filter__nav b-filter__nav-filters"
+        @click="filterDropdownVisible = !filterDropdownVisible"
       >
         <span>筛选</span>
         <!-- <svg><use xlink:href=""></use></svg> -->
       </a>
     </div>
-    <section class="b-filter__extend">
+
+    <section
+      class="b-filter__extend b-filter__extend-sorters"
+      :class="{ 'b-filter__extend_open': sortDropdownVisible }"
+    >
       <ul>
         <li v-for="(sorter, idx) in insideSorterList" :key="sorter.name + idx"
           @click="onSorterClick(sorter)"
-          :class="{ 'b-filter__sorter_active': checkSorterActive(sorter, selectedSorter) }"
+          class="b-filter__sorter-item"
+          :class="{ 'b-filter__sorter-item_active': checkSorterActive(sorter, selectedSorter) }"
         >
           <span>{{ sorter.name }}</span>
+          <img src="./selected.png" alt="">
         </li>
       </ul>
     </section>
-    <section class="b-filter__extend"></section>
-    <div class="console">filterPayload: {{ filterPayload }}</div>
+
+    <!--
+    <div class="console">
+      <p>filterPayload: {{ JSON.stringify(filterPayload) }}</p>
+    </div>
+     -->
   </aside>
 </template>
 
-
 <style lang="scss" scoped>
-  .b-filter {@at-root{
 
-    .b-filter__box {}
-    
+  .b-filter {}
+
+  /* base */
+
+    .b-filter__box {
+      position: relative;
+      height: 80px;
+    }
+
+    .b-filter__mask {
+      position: fixed;
+      z-index: 3;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background-color: rgba(0, 0, 0, .5);
+      transition: all .3s ease-in-out;
+
+      // backdrop-filter: blur(10px);
+    }
+
+  /* navbar */
+
     .b-filter__header {
+      position: relative;
+      z-index: 4;
       display: flex;
-      align-items: center;
       text-align: center;
+      background-color: #fff;
+      width: 100%;
+      border-bottom: 1px solid #ddd; /* no */
+      height: 79px;
+      line-height: 78px;
     }
     .b-filter__nav {
       display: block;
-      flex: 1 auto;
-      font-size: 32px;
-      line-height: 2em;
+      flex: 1;
+      white-space: nowrap;
+      text-align: center;
+      font-size: 28px;
+      color: #666;
+
       &_active {
         font-weight: 700;
         color: #000;
@@ -162,15 +281,66 @@
         font-weight: 700;
       }
     }
+    .b-filter__nav-filters {
+      flex: 0 auto;
+      width: 160px;
+    }
+
     .b-filter__extend {
-      li {
-        font-size: 32px;
-        line-height: 2em;
+      position: absolute;
+      z-index: 4;
+      left: 0;
+      right: 0;
+
+      overflow: auto;
+      border-top: 1px solid #ddd;
+      background-color: #fff;
+
+      /* collapse transition */
+      transition: all .2s ease-in-out;
+      opacity: 0;
+      visibility: hidden;
+      max-height: 0;
+      &_open {
+        opacity: 1;
+        visibility: visible;
+        max-height: 1500%;
       }
     }
-    .b-filter__sorter_active {
-      font-weight: 700;
-      color: #3190e8;
+
+  /* sorters panel */
+
+    .b-filter__extend-sorters {
+      padding: 16px 0 24px;
+      font-size: 28px;
+      color: #333;
+      background-color: #fff;
     }
-  }}
+    .b-filter__sorter-item {
+      position: relative;
+      padding-left: 40px;
+      font-size: 28px;
+      line-height: 80px;
+
+      img {
+        position: absolute;
+        right: 28px;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 30px;
+        height: 30px;
+
+        visibility: hidden;
+      }
+
+      &_active {
+        font-weight: 700;
+        color: #3190e8;
+        img {
+          visibility: visible;
+        }
+      }
+    }
+
+
 </style>
