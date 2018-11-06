b13d8b7e13a275fb79a25a04bcdc9ab44540c40d
Santochance <santochance@qq.com>
2018-11-06 09:10:17 +0800

    add placeholder for Discover and Order

--- src/views/Discover/Discover.vue
+++ src/views/Discover/Discover.vue
@@ -1,28 +1,47 @@
 <template>
   <Page title="发现" :back-btn="false">
-    Discover
+    <NoDataTip
+      class="p-discover__no-data"
+      :data="emptyTip"
+    ></NoDataTip>
   </Page>
 </template>
 
 <script>
   import Page from '@/components/Page'
+  import NoDataTip from '@/components/NoDataTip'
 
   export default {
     name: 'Discover',
     components: {
       Page,
+      NoDataTip,
     },
     props: {
-      
+
     },
     data() {
       return {
-        
+        emptyTip: {
+          img: 'https://fuss10.elemecdn.com/d/60/70008646170d1f654e926a2aaa3afpng.png',
+          title: '当前页面还在开发中',
+          button: {
+            text: '返回首页',
+            handler: () => {
+              this.$router.push('/')
+            }
+          }
+        }
       }
     },
   }
 </script>
 
 <style lang="scss" scoped>
-
+  .p-discover__no-data {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
--- src/views/Order/Order.vue
+++ src/views/Order/Order.vue
@@ -1,28 +1,60 @@
 <template>
-  <Page title="订单"  :back-btn="false">
-    Order
+  <Page title="订单" :back-btn="false">
+    <NoDataTip
+      class="p-order__no-data"
+      :data="emptyTip"
+    ></NoDataTip>
   </Page>
 </template>
 
 <script>
   import Page from '@/components/Page'
+  import NoDataTip from '@/components/NoDataTip'
 
   export default {
     name: 'Order',
     components: {
       Page,
+      NoDataTip,
     },
     props: {
-      
+
     },
     data() {
       return {
-        
+        // emptyTip: {
+        //   img: 'https://fuss10.elemecdn.com/d/60/70008646170d1f654e926a2aaa3afpng.png',
+        //   title: '登录后查看外卖订单',
+        //   button: {
+        //     text: '立即登录',
+        //     handler: () => {
+        //       this.$router.push({
+        //         path: '/login',
+        //         query: { redirect: this.$route.fullPath },
+        //       })
+        //     },
+        //   }
+        // }
+        emptyTip: {
+          img: 'https://fuss10.elemecdn.com/d/60/70008646170d1f654e926a2aaa3afpng.png',
+          title: '当前页面还在开发中',
+          button: {
+            text: '返回首页',
+            handler: () => {
+              this.$router.push('/')
+            }
+          }
+        }
       }
     },
   }
 </script>
 
 <style lang="scss" scoped>
-
+  .p-order__no-data {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
