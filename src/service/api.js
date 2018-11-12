81b192b7c581a2c7495b557a18575d65ce4af99c
Santochance <santochance@qq.com>
2018-11-12 18:55:30 +0800

    clean unused api and mock data

--- src/service/api.js
+++ src/service/api.js
@@ -137,35 +137,6 @@ export const fetchRatings = ({ restaurantId, has_content, tag_name, offset  }) =
   })}`)
 
 
-
-/**
- * 查询餐厅信息
- */
-export const fetchRestaurant = ({ restaurantId }) =>
-  request(`/restaurants/${restaurantId}`)
-
-/**
- * 查询餐厅菜单
- */
-export const fetchFoodMenu = (restaurantId) =>
-  request(`/restaurants/${restaurantId}/menu`)
-
-/**
- * 查询餐厅评分概况
- */
-export const fetchRatingOverview = (restaurantId) =>
-  request(`/restaurants/${restaurantId}/ratings`)
-
-/**
- * 查询餐厅评价
- */
-export const fetchComments = ({ restaurantId, tagName, offset, limit }) =>
-  request(`/restaurants/${restaurantId}/comments` + `?${stringify({
-    tag_name: tagName,
-    offset,
-    limit,
-  })}`)
-
 /**
  * 结算购物车
  */
