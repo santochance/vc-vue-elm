ce3ea87c9f3f974a612c6c9df34df7e0c7af8c15
Santochance <santochance@qq.com>
2018-12-02 19:59:56 +0800

    feat: FoodSpec support multiple specifications

--- src/views/Shop/OrderFood/FoodSpec.vue
+++ src/views/Shop/OrderFood/FoodSpec.vue
@@ -4,6 +4,18 @@
     <div class="specs__details">
       <ul class="specs__attrs">
         <li class="specs__attr"
+          v-for="(spec, index) in item.specifications" :key="spec.name + index"
+        >
+          <div class="specs__attr-title">{{ spec.name }}</div>
+          <ul class="specs__attr-options">
+            <li v-for="value in spec.values" :key="value"
+              class="specs__attr-option"
+              :class="{ 'specs__attr-option_active': selectedSpecs[index] && selectedSpecs[index].value === value }"
+              @click.stop.prevent="selectSpec(index, spec.name, value)"
+            >{{ value }}</li>
+          </ul>
+        </li>
+        <li class="specs__attr"
           v-for="(attr, index) in item.attrs" :key="attr.name + index"
         >
           <div class="specs__attr-title">{{ attr.name }}</div>
@@ -18,8 +30,8 @@
       </ul>
     </div>
     <div class="specs__result">
-      <div class="specs__price">{{ item.specfoods[0].price }}
-        <del class="specs__original-price">¥{{ item.specfoods[0].original_price }}</del>
+      <div class="specs__price">{{ item.specfoods[selectedSpecFoodIdx].price }}
+        <del class="specs__original-price">¥{{ item.specfoods[selectedSpecFoodIdx].original_price }}</del>
       </div>
       <a class="specs__btn-add-cart" href="javascript"
         @click.stop.prevent="ok">选好了</a>
@@ -44,6 +56,39 @@
     data () {
       return {
         selectedAttrs: [],
+        selectedSpecs: [],
+        selectedSpecFoodIdx: undefined,
+      }
+    },
+    computed: {
+      // specification idx -> specfoods idx
+      indexMap() {
+        let specs
+        try {
+          specs = this.item.specifications[0].values
+        } catch(e) {/*emtpy*/}
+
+        const result = specs.reduce((rst, spec) => {
+          // 搜索
+          let index = -1
+          this.item.specfoods.some((food, idx) => {
+            let foodspec
+            try {
+              foodspec = food.specs[0].value
+            } catch(e) {/* empty */}
+            if (spec === foodspec) {
+              index = idx
+              return true
+            }
+            return false
+          })
+
+          rst[spec] = index
+          return rst
+        }, {})
+
+        console.log('result:', result)
+        return result
       }
     },
     created() {
@@ -58,19 +103,26 @@
       ok () {
         this.$emit('ok', {
           ...this.item,
-          ...this.item.specfoods[0], // 如果有多种规格属性？
+          ...this.item.specfoods[this.selectedSpecFoodIdx], // 如果有多种规格属性？
           attrs: this.selectedAttrs,
         })
       },
       selectAttr(idx, name, value) {
         this.selectedAttrs.splice(idx, 1, { name, value })
       },
+      selectSpec(idx, name, value) {
+        this.selectedSpecs.splice(idx, 1, { name, value })
+        this.selectedSpecFoodIdx = this.indexMap[value]
+      },
       init() {
         // 如果item为null或undefined, 则退出
         const item = this.item
         if (!item) return
         const attrs = item.attrs || [];
+        const specs = item.specifications || [];
         this.selectedAttrs = attrs.map(attr => ({ name: attr.name, value: attr.values[0] }))
+        this.selectedSpecs = specs.map(spec => ({ name: spec.name, value: spec.values[0] }))
+        this.selectedSpecFoodIdx = 0
       }
     },
   }
