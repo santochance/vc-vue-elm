ef53191ff97d4c381e604d589f182bd4b61968b5
Santochance <santochance@qq.com>
2018-12-08 18:30:48 +0800

    chore: add Text component

---
+++ src/components/common/Text.vue
@@ -0,0 +1,106 @@
+<!--
+
+Exapmle:
+<Text :ellipsis="2"
+  :ellipsis-style="{ lineHeight: 1.2, backgroundColor: '#fff' }"
+>文本内容</Text>
+
+-->
+
+<template>
+  <span v-if="!ellipsis"><slot></slot></span>
+  <span v-else-if="ellipsis === 1" class="ellipsis"><slot></slot></span>
+  <span v-else :class="multiEllipsisClass" :style="multiEllipsisStyle"><slot></slot></span>
+</template>
+
+<script>
+  let lineClampSupported
+
+  export default {
+    name: 'Text',
+    prop: {
+      ellipsis: {
+        type: Number,
+        default: 0,
+      },
+      ellipsisStyle: {
+        type: Object,
+      }
+    },
+    data() {
+     return {
+
+     }
+    },
+    computed: {
+      multiEllipsisClass() {
+        if (!this.ellipsis) return ''
+        return lineClampSupported ? 'line-clamp' : 'pseudo-ellipsis'
+      },
+      multiEllipsisStyle() {
+        if (!this.ellipsis) return null
+        const lineCount = this.ellipsis
+        return lineClampSupported ? {
+          '-webkit-line-clamp': lineCount
+        } : this.resolveEllipsisStyle(lineCount, this.ellipsisStyle)
+      },
+    },
+    methods: {
+      resolveEllipsisStyle(lineCount, style) {
+        if (!lineCount) return null
+
+        const lineHeight = style.lineHeight
+        const bgColor = style.bgColor
+        if (!bgColor) console.warn(`
+          'background-color' should be set for browser not support '-webkit-line-clamp'
+        `)
+        return {
+          lineHeight: lineHeight,
+          maxHeight: lineHeight * lineCount + 'em',
+          backgroundColor: bgColor,
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .ellipsis {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .line-clamp {
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    /* -webkit-line-clamp: 3; */
+  }
+
+  .pseudo-ellipsis {
+    overflow: hidden;
+    /* line-height: 1.2; */
+    /* max-height: calc(1.2em * 3); */
+    position: relative;
+    text-align: justify;
+    margin-right: -1em;
+    padding-right: 1em;
+    /* set for :after inherit */
+    background: #fff;
+    &:before {
+      content: '...';
+      position: absolute;
+      right: 0;
+      bottom: 0;
+    }
+    &:after {
+      content: '...';
+      position: absolute;
+      right: 0;
+      color: transparent;
+      background: inherit;
+    }
+  }
+</style>
+
